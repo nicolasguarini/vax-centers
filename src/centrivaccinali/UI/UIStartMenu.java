165741b7a3d77f0ed3ecbaff9a297fbb5af9c69b
Dome309 <domy1riz@gmail.com>
2021-08-01 15:51:59 +0200

    StartMenu GUI updated

--- src/centrivaccinali/UI/UIStartMenu.java
+++ src/centrivaccinali/UI/UIStartMenu.java
@@ -12,18 +12,24 @@ import java.awt.event.ActionListener;
 import java.util.Objects;
 
 public class UIStartMenu extends JFrame implements ActionListener {
-    JLabel logo = new JLabel();
     ImageIcon img = new ImageIcon(Objects.requireNonNull(getClass().getResource("../../resources/images/logo.png")));
+    ImageIcon img2 = new ImageIcon(Objects.requireNonNull(getClass().getResource("../../resources/images/home.jpg")));
     JButton btnCentriVaccinali = new JButton("CENTRI VACCINALI");
     JButton btnCittadini = new JButton("CITTADINI");
     Border border = new LineBorder(new Color(251, 186, 0), 2, true);
 
     public UIStartMenu(){
-        logo.setIcon(img);
-        logo.setBounds(538, 140, 212, 212);
-        logo.setHorizontalTextPosition(JLabel.CENTER);
+        
+        JLabel labelLogo = new JLabel();
+        labelLogo.setIcon(img);
+		labelLogo.setBounds(535, 180, 212, 212);
 
-        btnCentriVaccinali.setBounds(415, 425, 200, 50);
+        JLabel labelTesto = new JLabel();
+        labelTesto.setText("Seleziona un metodo di accesso");
+        labelTesto.setFont(new Font("Montserrat", Font.BOLD, 15));
+		labelTesto.setBounds(520, 380, 300, 100);
+
+        btnCentriVaccinali.setBounds(425, 470, 200, 50);
         btnCentriVaccinali.setFocusable(false);
         btnCentriVaccinali.setFont(new Font("Montserrat", Font.BOLD, 15));
         btnCentriVaccinali.setBackground(new Color(232, 47, 125));
@@ -31,7 +37,7 @@ public class UIStartMenu extends JFrame implements ActionListener {
         btnCentriVaccinali.setBorder(border);
         btnCentriVaccinali.addActionListener(this);
 
-        btnCittadini.setBounds(675, 425, 200, 50);
+        btnCittadini.setBounds(655, 470, 200, 50);
         btnCittadini.setFocusable(false);
         btnCittadini.setFont(new Font("Montserrat", Font.BOLD, 15));
         btnCittadini.setBackground(new Color(232, 47, 125));
@@ -39,6 +45,28 @@ public class UIStartMenu extends JFrame implements ActionListener {
         btnCittadini.setBorder(border);
         btnCittadini.addActionListener(this);
 
+        JLabel labelSfondo = new JLabel();
+        labelSfondo.setIcon(img2);
+		labelSfondo.setBounds(0, 0, 1280, 720);
+        labelSfondo.add(labelTesto);
+        labelSfondo.add(btnCentriVaccinali);
+        labelSfondo.add(btnCittadini);
+        labelSfondo.add(labelLogo);
+
+		JPanel panelInfo = new JPanel();
+        panelInfo.setBounds(0, 0, 50, 50);
+		panelInfo.setLayout(null);
+        panelInfo.setBackground(Color.red);
+
+		
+		JPanel panelSfondo = new JPanel();
+		panelSfondo.setBounds(0, 0, 1280, 720);
+		panelSfondo.setLayout(null);
+        panelSfondo.add(labelSfondo);
+
+	
+		this.add(panelSfondo);
+    
         this.setTitle("Vax Centers");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(1280,720);
@@ -46,11 +74,6 @@ public class UIStartMenu extends JFrame implements ActionListener {
         this.setLocationRelativeTo(null);
         this.setIconImage(img.getImage());
         this.setResizable(false);
-
-        this.add(logo);
-        this.add(btnCentriVaccinali);
-        this.add(btnCittadini);
-
         this.setVisible(true);
     }
 
