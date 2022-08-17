4a901c9b5b11e6886d931a7178afbf1422e102cd
Nicolas Guarini <nikiguarincia@gmail.com>
2022-08-17 13:52:28 +0200

    Implemented 'Restart' and 'Stop' buttons in server dashboard

--- serverCV/src/main/java/serverCV/ServerCV.java
+++ serverCV/src/main/java/serverCV/ServerCV.java
@@ -1,17 +1,21 @@
 package serverCV;
 
-import com.sun.security.ntlm.Server;
 import serverCV.UI.UIStartMenu;
 
+import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.rmi.server.ExportException;
+import java.rmi.server.UnicastRemoteObject;
 
 public class ServerCV {
     public static final int PORT = 1099;
+    public static final String REGISTRY_NAME = "ServerCV";
 
     public static ServerImpl server;
     static Registry registry;
+    static boolean running = false;
 
     public static void main(String[] args){
         new UIStartMenu();
@@ -20,15 +24,38 @@ public class ServerCV {
     public static boolean startServer(){
         try{
             server = new ServerImpl();
-            registry = LocateRegistry.createRegistry(PORT);
-            registry.rebind("ServerCV", server);
-            System.out.println("Server ready!!");
+
+            try {
+                registry = LocateRegistry.createRegistry(PORT);
+            }catch (ExportException e){
+                registry = LocateRegistry.getRegistry(PORT);
+            }
+
+            registry.rebind(REGISTRY_NAME, server);
+            running = true;
         }catch(RemoteException e){
             e.printStackTrace();
             return false;
         }
 
         return true;
+    }
+
+    public static boolean stopServer(){
+        if(running){
+            try {
+                registry.unbind(REGISTRY_NAME);
+                UnicastRemoteObject.unexportObject(server, true);
+                running = false;
+                return true;
+            } catch (RemoteException | NotBoundException e) {
+                e.printStackTrace();
+            }
+
+            return false;
+        }else{
+            return true;
+        }
     }
 
     public static void addListener(LogListener toAdd){
--- serverCV/src/main/java/serverCV/UI/UIDashboard.java
+++ serverCV/src/main/java/serverCV/UI/UIDashboard.java
@@ -4,13 +4,20 @@ import serverCV.LogListener;
 import serverCV.ServerCV;
 
 import javax.swing.*;
+import javax.swing.border.LineBorder;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.util.Date;
 import java.util.Objects;
 
-public class UIDashboard extends JFrame implements LogListener {
+public class UIDashboard extends JFrame implements LogListener, ActionListener {
     JLabel labelStatus = new JLabel("Server is starting...");
     JTextArea logTextArea = new JTextArea();
+    JButton btnRestartServer = new JButton("RESTART");
+    JButton btnStopServer = new JButton("STOP");
     JScrollPane scrollPane;
 
     public UIDashboard(){
@@ -20,6 +27,32 @@ public class UIDashboard extends JFrame implements LogListener {
         labelStatus.setFont(fontBig);
         labelStatus.setBounds(50, 25, 1000, 50);
 
+        btnRestartServer.setPreferredSize(new Dimension(150, 40));
+        btnRestartServer.setFocusable(false);
+        btnRestartServer.setFont(new Font("Montserrat", Font.BOLD, 15));
+        btnRestartServer.setBackground(new Color(232, 47, 125));
+        btnRestartServer.setForeground(Color.WHITE);
+        btnRestartServer.setBorder(new LineBorder(new Color(251, 186, 0), 2, true));
+        btnRestartServer.addActionListener(this);
+        btnRestartServer.setBounds(50, 90, 170, 40);
+        btnRestartServer.setOpaque(true);
+
+        btnStopServer.setPreferredSize(new Dimension(150, 40));
+        btnStopServer.setFocusable(false);
+        btnStopServer.setFont(new Font("Montserrat", Font.BOLD, 15));
+        btnStopServer.setBackground(new Color(232, 47, 125));
+        btnStopServer.setForeground(Color.WHITE);
+        btnStopServer.setBorder(new LineBorder(new Color(251, 186, 0), 2, true));
+        btnStopServer.addActionListener(this);
+        btnStopServer.setBounds(250, 90, 170, 40);
+        btnStopServer.setOpaque(true);
+
+        logTextArea.setBounds(50, 150, 1000, 500);
+        logTextArea.setEditable(false);
+        logTextArea.setFont(fontSmall);
+        scrollPane = new JScrollPane(logTextArea, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        scrollPane.setBounds(50, 150, 1000, 500);
+
         this.setTitle("Vax Centers -- Server dashboard");
         this.setIconImage(imgLogo.getImage());
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -27,13 +60,10 @@ public class UIDashboard extends JFrame implements LogListener {
         this.setLayout(null);
         this.setResizable(false);
 
-        logTextArea.setBounds(50, 100, 1000, 500);
-        logTextArea.setEditable(false);
-        logTextArea.setFont(fontSmall);
-        scrollPane = new JScrollPane(logTextArea, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-        scrollPane.setBounds(50, 100, 1000, 500);
         this.add(scrollPane);
         this.add(labelStatus);
+        this.add(btnRestartServer);
+        this.add(btnStopServer);
 
         this.setLocationRelativeTo(null);
         this.setVisible(true);
@@ -45,6 +75,13 @@ public class UIDashboard extends JFrame implements LogListener {
         }else{
             labelStatus.setText("Error: unable to start the server");
         }
+
+        this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e){
+                ServerCV.stopServer();
+            }
+        });
     }
 
     @Override
@@ -54,5 +91,28 @@ public class UIDashboard extends JFrame implements LogListener {
 
         JScrollBar vertical = scrollPane.getVerticalScrollBar();
         vertical.setValue(vertical.getMaximum());
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == btnRestartServer){
+            updateLog(new Date() + " -- Restarting the server");
+            if(ServerCV.stopServer() && ServerCV.startServer()){
+                updateLog(new Date() + " -- Server restarted, now listening on port " + ServerCV.PORT);
+                labelStatus.setText("Server is ready, listening on port " + ServerCV.PORT + "!");
+            }else{
+                updateLog(new Date() + " -- Error: unable to restart the server.");
+                labelStatus.setText("Unable to restart the server.");
+            }
+        }else if(e.getSource() == btnStopServer){
+            updateLog(new Date() + " -- Stopping the server");
+            if(ServerCV.stopServer()){
+                updateLog(new Date() + " -- Server stopped");
+                labelStatus.setText("Server stopped.");
+            }else{
+                updateLog(new Date() + " -- Error: unable to stop the server.");
+                labelStatus.setText("Unable to stop the server.");
+            }
+        }
     }
 }
